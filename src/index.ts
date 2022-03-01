687fc6c38ebbfaa676398fa72374a9dd960bd4c6
Pau Baguer <pauinki@gmail.com>
2022-03-01 21:42:47 +0100

    added typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 8080;
 app.use(express.json());
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
-let tracklist = [
+interface Track {
+  id: String;
+  name: String;
+  author: String;
+}
+
+let tracklist: Track[] = [
   {
     id: "0",
     name: "La Barbacoa",
