8e1947826a441e19bf9cfc814fb200ff2b04e562
Pau Baguer <pauinki@gmail.com>
2022-03-10 09:48:19 +0100

    rem favs

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,14 +7,12 @@ export interface User {
   name: String;
   email: String;
   age: Number;
-  favouriteTrack: Track;
 }
 
 const userSchema = new Schema<User>({
   name: String,
   email: String,
   age: Number,
-  favouriteTrack: { type: Schema.Types.ObjectId, ref: "Track" },
 });
 
 export const UserModel = model<User>("User", userSchema);
